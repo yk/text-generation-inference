9de8dd15354a78230f56a309c434dd329ca68ad6
Yannic Kilcher <yk@users.noreply.github.com>
2023-01-27 12:57:49 +0100

    fixed build script

--- router/client/build.rs
+++ router/client/build.rs
@@ -1,6 +1,9 @@
 use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=../../proto/generate.proto");
+    println!("cargo:rerun-if-changed=src/pb/mod.rs");
+    println!("cargo:rerun-if-changed=src/pb/generate.v1.rs");
     fs::create_dir("src/pb").unwrap_or(());
     tonic_build::configure()
         .build_client(true)
